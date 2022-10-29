ff7cbb5d297da2c868cd2b90bfe8dbf40b3a0bb8
Richard Moreno <rich0194@gmail.com>
2022-10-29 01:21:33 -0300

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 document.getElementById("header").innerHTML = `
 
             <nav class="nav">
-                <img src="./IMG/logo2.png" alt="Logo pelis">
+                <img src="./imagenes/logo2.png" alt="Logo pelis">
                 <ul>
                     <li><a href="index2.html"> Home </a></li>
                     <li><a href="peliculas.html"> Top 10 </a></li>
@@ -14,13 +14,41 @@ document.getElementById("header").innerHTML = `
 
 `
 
+document.getElementById("footer").innerHTML = `
 
-document.getElementById("aca va el id del footer").innerHTML = `
-
-
+<footer class="footer">
+    <div class="bloqueFooter">
+        <div>
+            <h4 class="tituloFooter">Nuestro sitio</h4>
+            <ul class="menuFooter">
+                <li><a href="index2.html">Inicio</a></li>
+                <li><a href="peliculas.html">Top 10</a></li>
+                <li><a href="proximamente.html">Próximamente</a></li>
+                <li><a href="suscribite.html">Suscribite</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4 class="tituloFooter">Estamos con vos</h4>
+            <ul class="menuFooter">
+                <li><a href="index2.html">Inicio</a></li>
+                <li><a href="peliculas.html">Top10</a></li>
+                <li><a href="proximamente.html">Próximamente</a></li>
+                <li><a href="suscribite.html">Suscribite</a></li>
+            </ul>
+        </div>
+    <div>
+        <h4 class="tituloFooter">Enlaces relacionados</h4>
+        <ul class="menuFooter">
+            <li><a href="https://decine21.com/listas-de-cine/lista/las-mejores-peliculas-argentinas-100088">DeCine21</a></li>
+            <li><a href="https://www.filmin.es/catalogo/pais/argentina">Filmin</a></li>
+        </ul>
+    </div>
+    <div class="creditoFinal"> Desarrollado por Richard Moreno </div>
+    <a href="suscripcion" class="btn btn-primary" role="button">Suscribíte</a>
+    </form>
+</footer>
 
 
 `
-
 //-----------------------------------------------------------------------------------------------------------
 
